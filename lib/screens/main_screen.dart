f664874ceb426af950923bafa20e29096a93ca06
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-08-15 20:13:55 +0430

    travel_application_ui

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_travel_ui/screens/first_screen.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+
+import '../tools/colors.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    PersistentTabController _controller;
+
+    _controller = PersistentTabController(initialIndex: 0);
+    List<Widget> _buildScreens() {
+      return [
+        const FirstScreen(),
+        const FirstScreen(),
+        const FirstScreen(),
+        const FirstScreen(),
+      ];
+    }
+
+    List<PersistentBottomNavBarItem> _navBarsItems() {
+      return [
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.home),
+          inactiveIcon: const Icon(Icons.home_outlined),
+          activeColorPrimary: Colors.white,
+          inactiveColorPrimary: Colors.grey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.location_on),
+          inactiveIcon: const Icon(Icons.location_on_outlined),
+          activeColorPrimary: Colors.white,
+          inactiveColorPrimary: Colors.grey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.favorite),
+          inactiveIcon: const Icon(Icons.favorite_border_rounded),
+          activeColorPrimary: Colors.white,
+          inactiveColorPrimary: Colors.grey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.person),
+          inactiveIcon: const Icon(Icons.person_outline_rounded),
+          activeColorPrimary: Colors.white,
+          inactiveColorPrimary: Colors.grey,
+        ),
+      ];
+    }
+
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: navColor,
+      margin: EdgeInsets.symmetric(
+        horizontal: width * 0.03,
+        vertical: width * 0.02,
+      ),
+      bottomScreenMargin: 0.05,
+      // Default is Colors.white.
+      handleAndroidBackButtonPress: true,
+      // Default is true.
+      resizeToAvoidBottomInset: true,
+      // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+      stateManagement: true,
+      // Default is true.
+      hideNavigationBarWhenKeyboardShows: true,
+      // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(width * 0.06),
+        colorBehindNavBar: navColor,
+      ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: const ItemAnimationProperties(
+        // Navigation Bar's items animation properties.
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: const ScreenTransitionAnimation(
+        // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle:
+          NavBarStyle.style1, // Choose the nav bar style with this property.
+    );
+  }
+}
